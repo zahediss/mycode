896ebf5ff6f19427ecfa994f6fb3400b17de6d08
zahediss <sanamzah@gmail.com>
2020-07-14 18:18:34 -0400

    studying for logic

---
+++ introfor/customloop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for item in farms:
+    print(item)
+print("The list has ended")
+
+
