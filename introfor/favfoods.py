a3aadf87beff53fedbf4d95e2b64323ef3af0bb2
zahediss <sanamzah@gmail.com>
2020-07-15 10:35:12 -0400

    for loops challenge 2

---
+++ introfor/favfoods.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Create a list of your favorite foods (at least 5 items)
+
+foods = ["watermelon","cheeseburger","omelette","ramen"]
+
+# Iterate through that list with a for loop
+for x in foods:
+    print("I can't wait to eat some " + x + "!")
+print("\n Our loop has ended becasue I am so full!")
+
+# For each item in your list, print "I can't wait to eat some _____!"
