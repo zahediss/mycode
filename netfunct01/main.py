95f3228ba08e260d1384190e4f63ab48364be461
zahediss <sanamzah@gmail.com>
2020-07-15 16:09:07 -0400

    Learning about functions

---
+++ netfunct01/main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+
+# function to push commands
+def commandpush(devicecmd): # devicecmd==list
+    for coffeetime in devicecmd.keys():
+        print('Handshaking. .. ... connecting with ' + coffeetime )
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[coffeetime]:
+            print('Attempting to sending command --> ' + mycmds )
+            # we'll learn to write code that sends cmds to device here
+
+#create device reboot function
+#def devicerboot(iplist): ##iplist = list of IPs
+
+# start our main script
+def main():
+    work2do = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":  
+    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]} 
+    # data that replaces data stored in file
+
+    print("Welcome to the network device command pusher") # welcome message
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file
+
+    ## run
+    commandpush(work2do) # call function to push commands to devices
+    ## devicereboot(work2do)
+
+# call our main function
+main()
+          
+         
+          
+          
+          
+          
+          
+         
+          
+         
+          
+          
+         
+          
+          
+         
+          
+          
