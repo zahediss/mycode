10a4cbc0c383612d531c9410df332f0acb822676
zahediss <sanamzah@gmail.com>
2020-07-17 11:42:19 -0400

    requests method practice

---
+++ catfacts/random_cat_fact.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Author: RZFeeser@alta3.com"""
+
+# imports always go at the top of your code
+import requests
+import random
+
+def main():
+    """Run time code"""
+    ## create r, which is our request object
+    r = requests.get('https://cat-fact.herokuapp.com/facts')
+
+    ## catfact is our iterable -- that just means it will take on the values found within
+    ## r.json()["all"], one after the next-- which happens to be a dictionary
+    ## print a randomly chosen cat fact using random method
+    facts = r.json()
+    print(random.choice(facts["all"]))(["text"])
+
+main()
